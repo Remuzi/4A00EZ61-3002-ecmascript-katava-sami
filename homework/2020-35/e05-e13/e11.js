b8fafca37dc25852cd62e89bb8a5a5f99775a907
Remuzi <katava.sami@gmail.com>
2020-09-01 14:01:21 +0300

    added: e11

---
+++ homework/2020-35/e05-e13/e11.js
@@ -0,0 +1,11 @@
+var cars = ['BMW', 'Mercedes Benz', 'Chevrolet', 'VolksWagen']
+
+var i
+for (i = 0; i < cars.length; i++) {
+  console.log(cars[i])
+}
+
+var x
+for (x of cars) {
+  console.log(x)
+}
